082c9bf7432d5480c2a0aa72b64505084748666e
OlgaBarbina <75669779+OlgaBarbina@users.noreply.github.com>
2022-01-11 14:08:27 +0300

    Create cashe.py

---
+++ cashe.py
@@ -0,0 +1,11 @@
+class LRUCache:
+  def __init__(self,capacity):
+    pass
+  def get(self,key):
+    return 
+  def set(self,key,value):
+    dict_={'key':'value'}
+    pass
+  def rem(self,key):
+    pass
+  
